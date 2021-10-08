2571eda14e16ed3d96d5fd5b1c18a7b014267ee4
David Cabrera <de.cabrera1995@gmail.com>
2021-10-08 20:33:39 -0300

    Admin page

--- Views/admin-firstpage.php
+++ Views/admin-firstpage.php
@@ -1,4 +1,26 @@
-<?php 
-    echo "Admin login successful";
+<body id="page-top">
+        <!-- Navigation-->
+        <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
+        <nav id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand"><a href="#page-top">Menú</a></li>
+                <li class="sidebar-nav-item"><a href="#page-top">Empresas</a></li>
+                <li class="sidebar-nav-item"><a href="#about">Oportunidades</a></li>
+                <li class="sidebar-nav-item"><a href="#services">Perfil</a></li>
+                <li class="sidebar-nav-item"><a href="#portfolio">Estado</a></li>
+                <li class="sidebar-nav-item"><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <!-- Header-->
+        <header class="masthead d-flex align-items-center">
+            <div class="container px-4 px-lg-5 text-center">
+                <h1 class="mb-1">Bienvenido</h1>
+                <h2> Fuiste reconocido como Admin </h2>
+                <h3 class="mb-5"><em>En esta interfaz prodrás realizar las siguientes acciones</em></h3>
+                <a class="btn btn-primary btn-xl" href="#about">Ver Empresas</a>
+                <a class="btn btn-primary btn-xl" href="#about">Ver Alumnos</a>
+                <a class="btn btn-primary btn-xl" href="#about">Ver Propuestas</a>
+            </div>
+        </header>
 
-?>
+</body>
--- Views/student-firstpage.php
+++ Views/student-firstpage.php
@@ -1,3 +1,6 @@
+
+
+
 <?php 
     echo "Student login successful";
 
