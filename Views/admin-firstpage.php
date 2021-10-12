1f255dcaa1984e541867e5dba245a0eb1ff77844
David Cabrera <de.cabrera1995@gmail.com>
2021-10-12 18:50:59 -0300

    view adm and student

--- Views/admin-firstpage.php
+++ Views/admin-firstpage.php
@@ -1,26 +1,127 @@
-<body id="page-top">
-        <!-- Navigation-->
-        <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
-        <nav id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand"><a href="#page-top">Menú</a></li>
-                <li class="sidebar-nav-item"><a href="#page-top">Empresas</a></li>
-                <li class="sidebar-nav-item"><a href="#about">Oportunidades</a></li>
-                <li class="sidebar-nav-item"><a href="#services">Perfil</a></li>
-                <li class="sidebar-nav-item"><a href="#portfolio">Estado</a></li>
-                <li class="sidebar-nav-item"><a href="#contact">Contact</a></li>
+<!-- Navigation-->
+
+<body>
+    <!-- <div class="area"></div> -->
+    <nav class="main-menu">
+
+
+        <div class="menu-title">
+            <a href="#">
+                <i class="fa fa-home fa-2x"></i>
+                <h3 class="title">MENU</h3>
+            </a>
+        </div>
+
+        <ul>
+            <!-- <li>
+                <a href="#">
+                    <i class="fa fa-home fa-2x"></i>
+                    <h3>MENU</h3>
+                </a>
+
+            </li> -->
+            <li class="has-subnav">
+                <a href="#">
+                    <i class="fa fa-home fa-2x"></i>
+                    <span class="nav-text">
+                        Empresas
+                    </span>
+                </a>
+
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                    <i class="fa fa-laptop fa-2x"></i>
+                    <span class="nav-text">
+                        Propuestas
+                    </span>
+                </a>
+
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                    <i class="fa fa-list fa-2x"></i>
+                    <span class="nav-text">
+                        Alumnos
+                    </span>
+                </a>
+
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                    <i class="fa fa-folder-open fa-2x"></i>
+                    <span class="nav-text">
+                        Mensajes
+                    </span>
+                </a>
+
+            </li>
+            <!-- <li>
+                <a href="#">
+                    <i class="fa fa-bar-chart-o fa-2x"></i>
+                    <span class="nav-text">
+                        Graphs and Statistics
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-font fa-2x"></i>
+                    <span class="nav-text">
+                        Quotes
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-table fa-2x"></i>
+                    <span class="nav-text">
+                        Tables
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-map-marker fa-2x"></i>
+                    <span class="nav-text">
+                        Maps
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-info fa-2x"></i>
+                    <span class="nav-text">
+                        Documentation
+                    </span>
+                </a>
+            </li>
+        </ul> -->
+
+            <ul class="logout">
+                <li>
+                    <a href="#">
+                        <i class="fa fa-power-off fa-2x"></i>
+                        <span class="nav-text">
+                            Logout
+                        </span>
+                    </a>
+                </li>
             </ul>
-        </nav>
-        <!-- Header-->
-        <header class="masthead d-flex align-items-center">
-            <div class="container px-4 px-lg-5 text-center">
-                <h1 class="mb-1">Bienvenido</h1>
-                <h2> Fuiste reconocido como Admin </h2>
-                <h3 class="mb-5"><em>En esta interfaz prodrás realizar las siguientes acciones</em></h3>
-                <a class="btn btn-primary btn-xl" href="#about">Ver Empresas</a>
-                <a class="btn btn-primary btn-xl" href="#about">Ver Alumnos</a>
-                <a class="btn btn-primary btn-xl" href="#about">Ver Propuestas</a>
-            </div>
-        </header>
+    </nav>
+    <!--     
+
+    <!-- Header-->
+    <header class="masthead d-flex align-items-center">
+        <div class="container px-4 px-lg-5 text-center">
+        <!-- <div class="view-container"> -->
+            <h1 class="mb-1">Bienvenido</h1>
+            <h2> Fuiste reconocido como Admin </h2>
+            <h3 class="mb-5"><em>En esta interfaz prodrás realizar las siguientes acciones</em></h3>
+            <a class="btn btn-primary btn-xl" href="#">Ver Empresas</a>
+            <a class="btn btn-primary btn-xl" href="#t">Ver Alumnos</a>
+            <a class="btn btn-primary btn-xl" href="#">Ver Propuestas</a>
+        </div>
+    </header>
 
 </body>
--- Views/header.php
+++ Views/header.php
@@ -6,22 +6,14 @@
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-     <!-- Favicon-->
-     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-     <!-- Font Awesome icons (free version)-->
-     <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
-     <!-- Simple line icons-->
-     <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css" rel="stylesheet" />
-     <!-- Google fonts-->
-     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
-
+     
 
      <!-- Bootstrap CSS -->
      <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>bootstrap.min.css">
      <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>estilos.css">
      <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>styles.css">
 
-     <title>File Upload Example</title>
-</head>
+     <title>Bolsa de trabajo UTN</title>
 
-<body>
+}</head>
+
