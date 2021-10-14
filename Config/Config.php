f2bd1017916e67bada522a7d3c2db0c7c848d0b2
celemasmut <celestemasmut@gmail.com>
2021-10-13 21:40:45 -0300

    show company - student

--- Config/Config.php
+++ Config/Config.php
@@ -2,8 +2,8 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //define("FRONT_ROOT", "http://localhost/TPFINAL/LabIV---Proyecto-Final-2021/");//David
-//define("FRONT_ROOT", "/Lab4/practicaTPfinal/");
-define("FRONT_ROOT", "http://localhost/Projecto%20Final/");
+define("FRONT_ROOT", "http://localhost:8080/Lab4/LabIV---Proyecto-Final-2021/");
+//define("FRONT_ROOT", "http://localhost/Projecto%20Final/");
 define("VIEWS_PATH", "Views/");
 define("UPLOADS_PATH", "Uploads/");
 define("CSS_PATH", FRONT_ROOT . VIEWS_PATH . "css/");
