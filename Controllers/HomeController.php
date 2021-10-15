7f6df3ef29bcc005c32a57d48124b4b462e57895
celemasmut <celestemasmut@gmail.com>
2021-10-15 13:19:57 -0300

    perfil alumno

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Models\User as User;
 use Controllers\StudentController as StudentController;
 use Utils\Utils as Utils;
 use Models\Student as Student;
+use DAO\CareerDAO as CareerDAO;
 
 class HomeController
 {
@@ -26,11 +27,16 @@ class HomeController
             $studentController = new StudentController();
             $student = new Student();
             $student = $studentController->getByEmail($email);
+            $careerDAO = new CareerDAO();
+            $career = $careerDAO->getCareerStudent($student);
+
 
             if ($student != null) {
                 $_SESSION['student'] = $student;
-
                 require_once(VIEWS_PATH . "student-firstpage.php");
+                require_once(VIEWS_PATH . "student-show.php");
+                
+
             } else {
                 $invalidEmail = true;
                 require_once(VIEWS_PATH . "index.php");
