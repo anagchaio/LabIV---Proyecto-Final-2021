ef5904d5b6b9f593d83fded98e4f4900ee66afc7
DavidEmanuelCabrera <davidemmanuelcabrera@gmail.com>
2021-11-16 22:17:20 -0300

    jobOffer view company

--- Views/jobOffer-list.php
+++ Views/jobOffer-list.php
@@ -4,6 +4,8 @@ use Models\JobOffer;
 
 if (isset($_SESSION['admin'])) {
     require_once('nav.php');
+} else if (isset($_SESSION['company'])) {
+    require_once('nav-company.php');
 } else {
     require_once('nav-student.php');
 }
@@ -17,65 +19,67 @@ if (isset($_SESSION['admin'])) {
             <h2 class="mb-4">Ofertas</h2>
             <div class="flex">
                 <div class="form-group-input">
-
-                    <form action="<?php echo FRONT_ROOT ?>JobOffer/ShowListView" method="GET">
-                        <select name="careerId" class="form-control form-control-ml" style="width: 130vh;">
-                            <?php
-                            echo "<option value=" . 0 . ">Todas</option>";
-                            if (isset($careers)) {
-                                foreach ($careers as $career) {                                    
-                                    if(isset($careerId) && $career->getCareerId() == $careerId){
-                                        echo "<option selected value=" . $career->getCareerId() . ">" . $career->getDescription() . "</option>";
-                                    } else {
-                                        echo "<option value=" . $career->getCareerId() . ">" . $career->getDescription() . "</option>";
+                    <?php if (isset($_SESSION['admin']) || isset($_SESSION['student'])) { ?>
+                        <form action="<?php echo FRONT_ROOT ?>JobOffer/ShowListView" method="GET">
+                            <select name="careerId" class="form-control form-control-ml" style="width: 130vh;">
+                                <?php
+                                echo "<option value=" . 0 . ">Todas</option>";
+                                if (isset($careers)) {
+                                    foreach ($careers as $career) {
+                                        if (isset($careerId) && $career->getCareerId() == $careerId) {
+                                            echo "<option selected value=" . $career->getCareerId() . ">" . $career->getDescription() . "</option>";
+                                        } else {
+                                            echo "<option value=" . $career->getCareerId() . ">" . $career->getDescription() . "</option>";
+                                        }
                                     }
                                 }
-                            }?>
-                        </select>
+
+                                ?>
+                            </select>
                 </div>
-                
+
                 <div style="margin-left: 103vh;">
                     <button type="submit" name="filter-button" class="btn btn-primary">Filtrar</button>
                 </div>
 
                 </form>
+            <?php } ?>
             </div>
             <table class="table table-hover">
-            <?php
-            
-            if (isset($jobOffers) && $jobOffers != null) {
-                
-                echo "<thead>";
-                echo " <th>Empresa</th>";
-                echo " <th>Description</th>";
-                echo " <th>Position</th>";
-                echo " <th>Carrera</th>";
-                echo " <th>Fecha limite</th>";
-                echo " <th>Estado</th>";
-                echo " <th>Ver</th>";
-                echo " </thead>";
-                echo " <tbody>";
+                <?php
 
-                foreach ($jobOffers as $jobOffer) {
-                    echo  "<tr>";
-                    echo  "<td>" . $jobOffer->getCompany_name() . "</td>";
-                    echo  "<td>" . $jobOffer->getJobOffer_description() . "</td>";
-                    echo  "<td>" . $jobOffer->getJobPosition_description() . "</td>";
-                    echo  "<td>" . $jobOffer->getCareer_description() . "</td>";
-                    echo  "<td>" . $jobOffer->getLimitDate() . "</td>";
-                    echo  "<td>" . $jobOffer->getState() . "</td>";
+                if (isset($jobOffers) && $jobOffers != null) {
 
-                    $jobOfferId = $jobOffer->getJobOfferId();
-                    echo "<td><a href=" . FRONT_ROOT . "JobOffer/ShowOffer/" . $jobOfferId . ">+ info</a></td>";
+                    echo "<thead>";
+                    echo " <th>Empresa</th>";
+                    echo " <th>Description</th>";
+                    echo " <th>Position</th>";
+                    echo " <th>Carrera</th>";
+                    echo " <th>Fecha limite</th>";
+                    echo " <th>Estado</th>";
+                    echo " <th>Ver</th>";
+                    echo " </thead>";
+                    echo " <tbody>";
+
+                    foreach ($jobOffers as $jobOffer) {
+                        echo  "<tr>";
+                        echo  "<td>" . $jobOffer->getCompany_name() . "</td>";
+                        echo  "<td>" . $jobOffer->getJobOffer_description() . "</td>";
+                        echo  "<td>" . $jobOffer->getJobPosition_description() . "</td>";
+                        echo  "<td>" . $jobOffer->getCareer_description() . "</td>";
+                        echo  "<td>" . $jobOffer->getLimitDate() . "</td>";
+                        echo  "<td>" . $jobOffer->getState() . "</td>";
+
+                        $jobOfferId = $jobOffer->getJobOfferId();
+                        echo "<td><a href=" . FRONT_ROOT . "JobOffer/ShowOffer/" . $jobOfferId . ">+ info</a></td>";
+                    }
+                } else {
+                    echo "<h4>";
+                    echo "No hay ofertas para mostrar de esta carrera";
+                    echo "</h4>";
                 }
-            } else {
-                echo "<h4>";
-                echo "No hay ofertas para mostrar de esta carrera";
-                echo "</h4>";
-            
-            }
-            ?>
-            </tbody>
+                ?>
+                </tbody>
 
             </table>
         </div>
